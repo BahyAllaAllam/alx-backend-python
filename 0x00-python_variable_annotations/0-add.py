e2bf69a02595862dc41b5e9a7d1ce0681c299e71
Bahy Allam <66816838+BahyAllaAllam@users.noreply.github.com>
2024-07-05 02:16:15 +0300

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Add two floating-point
+numbers and return their sum.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floating-point
+    numbers and return their sum.
+    """
